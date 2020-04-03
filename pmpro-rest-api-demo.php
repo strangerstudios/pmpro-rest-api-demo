5e27b08578c3bd6c98c45936356707464fb775e3
Jason Coleman <jason@strangerstudios.com>
2020-04-03 11:25:34 -0400

    More endpoints.

--- pmpro-rest-api-demo.php
+++ pmpro-rest-api-demo.php
@@ -123,3 +123,133 @@ function pmprorad_get_membership_level_for_user( $user_id ) {
 	return $membership_level;
 }
 
+/**
+ * Get all membership levels (MMPU) for a user.
+ * /wp-json/pmpro/v1/get_membership_levels_for_user
+ *
+ * You must pass either a user_id OR email param to the endpoint.
+ */
+function pmprorad_get_membership_levels_for_user( $user_id ) {
+	$endpoint = '/wp-json/pmpro/v1/get_membership_levels_for_user';
+	
+	if ( is_numeric( $user_id ) ) {
+		$params = array( 'user_id' => $user_id );
+	} else {
+		$params = array( 'email' => $user_id );
+	}
+	
+	$membership_levels = pmprorad_request( $endpoint, 'get', $params );
+	
+	return $membership_levels;
+}
+
+/**
+ * Get a user's access for a specific post.
+ * /wp-json/pmpro/v1/has_membership_access
+ *
+ * You must pass either a user_id OR email param to the endpoint.
+ * You must pass a post_id param to the endpoint.
+ */
+function pmprorad_get_membership_levels_for_user( $user_id, $post_id ) {
+	$endpoint = '/wp-json/pmpro/v1/has_membership_access';
+	
+	if ( is_numeric( $user_id ) ) {
+		$params = array( 'user_id' => $user_id, 'post_id' => $post_id );
+	} else {
+		$params = array( 'email' => $user_id, 'post_id' => $post_id  );
+	}
+	
+	$has_access = pmprorad_request( $endpoint, 'get', $params );
+	
+	return $has_access;
+}
+
+/**
+ * Change a member's level.
+ * /wp-json/pmpro/v1/change_membership_level
+ *
+ * You must pass either a user_id OR email param to the endpoint.
+ * You must pass a level_id param to the endpoint.
+ */
+function pmprorad_change_membership_level( $user_id, $level_id ) {
+	$endpoint = '/wp-json/pmpro/v1/change_membership_level';
+	
+	if ( is_numeric( $user_id ) ) {
+		$params = array( 'user_id' => $user_id, 'level_id' => $level_id );
+	} else {
+		$params = array( 'email' => $user_id, 'level_id' => $level_id );
+	}
+	
+	$changed = pmprorad_request( $endpoint, 'post', $params );
+	
+	return $changed;
+}
+
+/**
+ * Cancel a member's level.
+ * /wp-json/pmpro/v1/change_membership_level
+ *
+ * You must pass either a user_id OR email param to the endpoint.
+ * You must pass a level_id param to the endpoint.
+ */
+function pmprorad_cancel_membership_level( $user_id, $level_id ) {
+	$endpoint = '/wp-json/pmpro/v1/cancel_membership_level';
+	
+	if ( is_numeric( $user_id ) ) {
+		$params = array( 'user_id' => $user_id, 'level_id' => $level_id );
+	} else {
+		$params = array( 'email' => $user_id, 'level_id' => $level_id );
+	}
+	
+	$cancelled = pmprorad_request( $endpoint, 'post', $params );
+	
+	return $cancelled;
+}
+
+/**
+ * Get data for a membership level.
+ * /wp-json/pmpro/v1/membership_level
+ *
+ * You must pass the level_id as a param to the endpoint.
+ */
+function pmprorad_get_membership_level( $level_id ) {
+	$endpoint = '/wp-json/pmpro/v1/membership_level';
+	
+	$params = array( 'id' => $level_id );	
+	
+	$membership_level = pmprorad_request( $endpoint, 'get', $params );
+	
+	return $membership_level;
+}
+
+/**
+ * Delete a membership level.
+ * /wp-json/pmpro/v1/membership_level
+ *
+ * You must pass the level_id as a param to the endpoint.
+ */
+function pmprorad_delete_membership_level( $level_id ) {
+	$endpoint = '/wp-json/pmpro/v1/membership_level';
+	
+	$params = array( 'id' => $level_id );	
+	
+	$membership_level = pmprorad_request( $endpoint, 'delete', $params );
+	
+	return $membership_level;
+}
+
+/**
+ * Get data for a discount code.
+ * /wp-json/pmpro/v1/discount_code
+ *
+ * You must pass the code as a param to the endpoint.
+ */
+function pmprorad_get_discount_code( $code ) {
+	$endpoint = '/wp-json/pmpro/v1/discount_code';
+	
+	$params = array( 'code' => $code );
+	
+	$discount_code = pmprorad_request( $endpoint, 'get', $params );
+	
+	return $discount_code;
+}
